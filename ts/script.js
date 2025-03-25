539404382e1f71d5f7ba9f692c9faac02ac28ea6
carloscMe <carloscarrillo.sprout@gmail.com>
2025-03-25 17:00:50 -0500

    prueba-1

--- ts/script.js
+++ ts/script.js
@@ -1,3 +1,29 @@
 "use strict";
-console.log("hello word!");
-alert("hola papu");
+const texto = document.querySelector("#texto");
+const pequeño = document.querySelector("#pequeño");
+const mediano = document.querySelector("#mediano");
+const grande = document.querySelector("#grande");
+const entrada = document.querySelector("#entrada");
+const texto2 = document.querySelector(".texto--2");
+const button = document.querySelector("button");
+pequeño === null || pequeño === void 0 ? void 0 : pequeño.addEventListener("click", () => {
+    texto.style.fontSize = ".9rem"; /* reto: dependiendo de la opcion al recargar la pagina se quede esa opcion*/
+});
+mediano === null || mediano === void 0 ? void 0 : mediano.addEventListener("click", () => {
+    texto.style.fontSize = "2rem";
+});
+grande === null || grande === void 0 ? void 0 : grande.addEventListener("click", () => {
+    texto.style.fontSize = "3rem";
+});
+button.addEventListener("click", validar);
+function validar() {
+    if (entrada.value) {
+        texto2.innerText = entrada.value;
+        entrada.value = '';
+    }
+    else {
+        alert("escribe algo");
+    }
+    ;
+}
+;
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,35 @@
-console.log("hello word!");
+const texto= document.querySelector("#texto") as HTMLParagraphElement;
+const pequeño = document.querySelector("#pequeño") as HTMLInputElement;
+const mediano = document.querySelector("#mediano") as HTMLInputElement;
+const grande = document.querySelector("#grande") as HTMLInputElement;
 
-alert("hola papu");
+const entrada = document.querySelector("#entrada") as HTMLInputElement;
+const texto2 = document.querySelector(".texto--2") as HTMLParagraphElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+
+pequeño?.addEventListener("click", () =>{
+    texto.style.fontSize = ".9rem"                     /* reto: dependiendo de la opcion al recargar la pagina se quede esa opcion*/
+});
+
+mediano?.addEventListener("click", () =>{
+    texto.style.fontSize = "2rem"
+});
+
+grande?.addEventListener("click", () =>{
+    texto.style.fontSize = "3rem"
+});
+
+button.addEventListener("click", validar);
+
+function validar () {
+
+    if(entrada.value){
+        texto2.innerText = entrada.value;
+        entrada.value = '';
+    }
+
+    else {
+        alert("escribe algo");
+    };
+   
+};
